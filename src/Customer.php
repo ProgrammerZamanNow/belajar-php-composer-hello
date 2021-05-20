9c8d675e5bae69cdc4076cde5961fee6292635d0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-05-20 22:47:44 +0700

    Add default value

--- src/Customer.php
+++ src/Customer.php
@@ -9,7 +9,7 @@ class Customer
     {
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name = "Guest"): string
     {
         return "Hello $name, My Name is $this->name";
     }
